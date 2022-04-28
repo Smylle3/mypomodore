aebafd0f05c02324b69ee703f2c47c353f70c226
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-28 14:34:02 -0300

    Remove Material Provider from app

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from '@mui/material'
 import App from './App'
-import { theme } from 'themes/globalThemes'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 )
