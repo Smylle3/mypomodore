bb2bc708590a65fdd59886d153c27a7dc26fd60e
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-28 14:33:04 -0300

    Add chakra provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from 'themes/globalThemes'
+import Home from 'home'
+
 export default function App() {
     return (
-        <div>
-            <p>Edit</p>
-        </div>
+        <ChakraProvider theme={theme}>
+            <Home />
+        </ChakraProvider>
     )
 }
