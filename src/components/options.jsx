756c11f224dcdbffcd4a64f2c37e3ad672eb8ac3
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-29 18:44:39 -0300

    Create a component for modal configurations

---
+++ src/components/options.jsx
@@ -0,0 +1,43 @@
+import {
+    Button,
+    Flex,
+    FormControl,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Text
+} from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import MyInput from './myInput'
+
+export default function Options(props) {
+    useEffect(() => props.onOpen, [props.onOpen])
+
+    return (
+        <Modal isOpen={props.isOpen} onClose={props.onClose}>
+            <ModalOverlay />
+            <ModalContent borderRadius={25}>
+                <ModalHeader>Settings</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Flex direction="column">
+                        <Text>TIME (MINUTES)</Text>
+                        <FormControl display="flex" flexDirection="row">
+                            <MyInput id="pomodoro" number={25} />
+                            <MyInput id="break" number={5} />
+                        </FormControl>
+                    </Flex>
+                </ModalBody>
+                <ModalFooter display="flex" justifyContent="center">
+                    <Button colorScheme="green" borderRadius={10} onClick={props.onClose}>
+                        Apply
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
