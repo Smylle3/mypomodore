3fba57be3ea54360ab0ae333e032edb3572927d2
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-29 18:45:50 -0300

    Add config button to users sets your times

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import {
-    Flex,
-    Heading
-} from '@chakra-ui/react'
+import { Flex, Heading, IconButton, useDisclosure } from '@chakra-ui/react'
+import { BsFillGearFill } from 'react-icons/bs'
 import Circles from 'components/circles'
 import Timer from 'components/timer'
+import Options from 'components/options'
+import { useAuth } from 'context/context'
 
 export default function Home() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { setUserPomodoro, setUserBreak } = useAuth()
+
     return (
         <Flex
             w="100vw"
@@ -24,10 +27,22 @@ export default function Home() {
             >
                 POMODORO
             </Heading>
+            <IconButton
+                variant="ghost"
+                colorScheme="blackAlpha"
+                icon={<BsFillGearFill color="white" />}
+                onClick={onOpen}
+            />
             <Circles>
                 <Timer />
             </Circles>
-            
+            <Options
+                isOpen={isOpen}
+                onOpen={onOpen}
+                onClose={onClose}
+                setUserBreak={setUserBreak}
+                setUserPomodoro={setUserPomodoro}
+            />
         </Flex>
     )
 }
