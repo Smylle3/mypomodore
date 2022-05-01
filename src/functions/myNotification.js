6e99fbf2bd0c8de5e2e257ec56946798e8cf23ac
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-30 23:11:20 -0300

    Removes temporaly notifications from app

--- src/functions/myNotification.js
+++ src/functions/myNotification.js
@@ -19,6 +19,6 @@ export function MyNotification(title, message) {
 }
 
 function IsNotify(title, message) {
-    new Notification('titulo')
-    //new Audio(soundNotification).play()
+    new Notification(title, { body: message, icon: iconNotification })
+    new Audio(soundNotification).play()
 }
