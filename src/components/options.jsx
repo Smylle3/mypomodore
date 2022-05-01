3f556407cf0c56b38cf6998b453d3e7f1d7de4e2
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-01 12:46:22 -0300

    Create a switch to show if notifications is enable

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -2,6 +2,7 @@ import {
     Button,
     Flex,
     FormControl,
+    FormLabel,
     Modal,
     ModalBody,
     ModalCloseButton,
@@ -9,12 +10,41 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
+    Switch,
     Text
 } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import MyInput from './myInput'
 
 export default function Options(props) {
+    const [enableNotify, setEnableNotify] = useState(false)
+    const handleEnable = () => {
+        if (Notification.permission === 'granted') {
+            Notification.requestPermission((response) => {
+                if (response === 'granted') {
+                    setEnableNotify(true)
+                } else {
+                    setEnableNotify(false)
+                }
+            })
+        } else if (
+            Notification.permission === 'denied' ||
+            Notification.permission === 'default'
+        ) {
+            Notification.requestPermission((response) => {
+                if (response === 'granted') {
+                    setEnableNotify(true)
+                } else {
+                    setEnableNotify(false)
+                }
+            })
+        }
+    }
+
+    useEffect(() => {
+        setEnableNotify(Notification.permission === 'granted')
+    }, [])
+
     useEffect(() => props.onOpen, [props.onOpen])
 
     return (
@@ -31,7 +61,26 @@ export default function Options(props) {
                             <MyInput id="break" number={5} />
                         </FormControl>
                         {window.Notification ? (
-                            Notification.permission
+                            <>
+                                <FormControl
+                                    display="flex"
+                                    alignItems="center"
+                                    marginTop={5}
+                                >
+                                    <FormLabel
+                                        htmlFor="email-alerts"
+                                        mb="0"
+                                        onClick={() => handleEnable()}
+                                    >
+                                        Enable notifications?
+                                    </FormLabel>
+                                    <Switch
+                                        colorScheme="orange"
+                                        isChecked={enableNotify}
+                                        onChange={() => handleEnable()}
+                                    />
+                                </FormControl>
+                            </>
                         ) : (
                             <Text color="red" margin="1em 1em 0em 0">
                                 This browser doesn't have support for notifications.
