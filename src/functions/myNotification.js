6087e54eb29319f4d53eac7978f56369b18a8fc1
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-30 22:50:06 -0300

    Create a function to notify users when time's over

---
+++ src/functions/myNotification.js
@@ -0,0 +1,24 @@
+import soundNotification from '../assets/audio.mp3'
+import iconNotification from '../assets/icon.png'
+
+export function MyNotification(title, message) {
+    if ('Notification' in window) {
+        if (Notification.permission === 'granted') {
+            IsNotify(title, message)
+        } else if (
+            Notification.permission === 'denied' ||
+            Notification.permission === 'default'
+        ) {
+            Notification.requestPermission((response) => {
+                if (response === 'granted') {
+                    IsNotify(title, message)
+                }
+            })
+        }
+    }
+}
+
+function IsNotify(title, message) {
+    new Notification(title, { body: message, icon: iconNotification })
+    new Audio(soundNotification).play()
+}
